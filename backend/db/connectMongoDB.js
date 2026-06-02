6dc75216a48d761790b4814634a4495026ac25ce
Corne Carelse <codingcorne@gmail.com>
2026-06-02 10:49:57 +0200

    Update project

--- backend/db/connectMongoDB.js
+++ backend/db/connectMongoDB.js
@@ -2,9 +2,11 @@ import mongoose from "mongoose";
 
 export default async function connectMongoDB() {
 	try {
+		console.log("MONGO_URI:", process.env.MONGO_URI);
+
 		await mongoose.connect(process.env.MONGO_URI);
 		console.log("MONGODB connected");
 	} catch (error) {
-		console.log("Error connecting to MongoDB: ", error.message);
+		console.log("Error connecting to MongoDB:", error.message);
 	}
 }
